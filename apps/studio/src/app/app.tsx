1ef916ffc824e159b9f60c7fdb863acb40a96c36
Gérôme Grignon <gerome.grignon.lp2@gmail.com>
2023-09-24 16:16:32 +0200

    refactor(studio): remove boilerplate

--- apps/studio/src/app/app.tsx
+++ apps/studio/src/app/app.tsx
@@ -1,14 +1,8 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from './app.module.css';
 
-import NxWelcome from './nx-welcome';
-
 export function App() {
-  return (
-    <div>
-      <NxWelcome title="studio" />
-    </div>
-  );
+  return <></>;
 }
 
 export default App;
