80b193ad73cf466c4b06dd47e10c99bac3a3209d
Gérôme Grignon <gerome.grignon.lp2@gmail.com>
2023-09-24 15:57:19 +0200

    chore: update commitlint scopes

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -15,7 +15,7 @@ const Configuration: UserConfig = {
    * Any rules defined here will override rules from @commitlint/config-conventional
    */
   rules: {
-    'scope-enum': [2, 'always', ['core', 'ui', 'api', 'docs', 'test']],
+    'scope-enum': [2, 'always', ['core', 'studio', 'api', 'docs']],
   },
   /*
    * Functions that return true if commitlint should ignore the given message.
