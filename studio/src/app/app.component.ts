118f2d82c149b7886d6650d32fd2de7e08c318fc
Gerome Grignon <gerome.grignon.lp2@gmail.com>
2023-10-08 01:06:57 +0200

    feat(studio): add audio/cam toggle

--- studio/src/app/app.component.ts
+++ studio/src/app/app.component.ts
@@ -1,6 +1,8 @@
 import {Component} from '@angular/core';
 import {RouterModule} from '@angular/router';
 import {HeaderComponent} from "./core/components/header.component";
+import {MatIconRegistry} from "@angular/material/icon";
+import {DomSanitizer} from "@angular/platform-browser";
 
 @Component({
   standalone: true,
@@ -14,4 +16,10 @@ import {HeaderComponent} from "./core/components/header.component";
   `,
 })
 export class AppComponent {
+  constructor(iconRegistry: MatIconRegistry, sanitizer: DomSanitizer) {
+    iconRegistry.addSvgIcon('volume_off', sanitizer.bypassSecurityTrustResourceUrl('assets/icons/volume_off.svg'));
+    iconRegistry.addSvgIcon('volume', sanitizer.bypassSecurityTrustResourceUrl('assets/icons/volume.svg'));
+    iconRegistry.addSvgIcon('videocam', sanitizer.bypassSecurityTrustResourceUrl('assets/icons/videocam.svg'));
+    iconRegistry.addSvgIcon('videocam_off', sanitizer.bypassSecurityTrustResourceUrl('assets/icons/videocam_off.svg'));
+  }
 }
--- studio/src/app/pages/record/record.component.ts
+++ studio/src/app/pages/record/record.component.ts
@@ -1,11 +1,12 @@
-import {AfterViewInit, Component, ElementRef, signal, ViewChild} from "@angular/core";
+import {AfterViewInit, Component, computed, ElementRef, signal, ViewChild} from "@angular/core";
 import {getSupportedMimeTypes} from "../../shared/utils/mime-type.util";
 import {defer} from "rxjs";
-import {takeUntilDestroyed} from "@angular/core/rxjs-interop";
 import {getAudioInputDevices, getVideoDevices} from "../../shared/utils/media-devices.util";
 import {MatFormFieldModule} from "@angular/material/form-field";
 import {MatSelectModule} from "@angular/material/select";
 import {AsyncPipe, NgForOf} from "@angular/common";
+import {MatButtonModule} from "@angular/material/button";
+import {MatIconModule} from "@angular/material/icon";
 
 @Component({
   selector: 'beever-record',
@@ -14,69 +15,86 @@ import {AsyncPipe, NgForOf} from "@angular/common";
     MatFormFieldModule,
     MatSelectModule,
     AsyncPipe,
-    NgForOf
+    NgForOf,
+    MatButtonModule,
+    MatIconModule
   ],
   template: `
-    <video
-      #video
-      class="rounded-lg border-2 border-primary p-2 bg-black"
-      width="640"
-      height="480"
-      playsInline
-      autoPlay
-      muted>
-    </video>
-    <section class="flex items-center justify-center gap-4 p-2">
-      <button
-        class="border-2 border-gray-500 rounded-xl px-2 py-2"
-        title="Start recording"
-        (click)="startRecording()"
-      >
-        <img src="assets/icons/play_arrow.svg" alt=""/>
-      </button>
-      <button
-        class="border-2 border-gray-500 rounded-xl px-2 py-2"
-        title="Stop recording"
-        (click)="stopRecording()"
-      >
-        <img src="assets/icons/stop.svg" alt=""/>
-      </button>
-      <button
-        class="border-2 border-gray-500 rounded-xl px-2 py-2"
-        title="Download recording"
-        (click)="downloadRecording()"
-      >
-        <img src="assets/icons/download.svg" alt=""/>
-      </button>
-    </section>
-    <form class="flex flex-col">
-      <mat-form-field>
-        <mat-label>Video device</mat-label>
-        <mat-select>
-          <mat-option *ngFor="let device of videoDevices$ | async" [value]="device.deviceId">
-            {{device.label}}
-          </mat-option>
-        </mat-select>
-      </mat-form-field>
+      <video
+              #video
+              class="rounded-lg border-2 border-primary p-2 bg-black"
+              width="640"
+              height="480"
+              playsInline
+              autoPlay
+              muted>
+      </video>
+      <section class="flex items-center justify-center gap-4 p-2">
+          <button
+                  class="border-2 border-gray-500 rounded-xl px-2 py-2"
+                  title="Start recording"
+                  (click)="startRecording()"
+          >
+              <img src="assets/icons/play_arrow.svg" alt=""/>
+          </button>
+          <button
+                  class="border-2 border-gray-500 rounded-xl px-2 py-2"
+                  title="Stop recording"
+                  (click)="stopRecording()"
+          >
+              <img src="assets/icons/stop.svg" alt=""/>
+          </button>
+          <button
+                  class="border-2 border-gray-500 rounded-xl px-2 py-2"
+                  title="Download recording"
+                  (click)="downloadRecording()"
+          >
+              <img src="assets/icons/download.svg" alt=""/>
+          </button>
+      </section>
+      <form class="flex flex-col">
+          <div class="flex gap-2">
+              <mat-form-field class="flex-grow">
+                  <mat-label>Video device</mat-label>
+                  <mat-select>
+                      <mat-option *ngFor="let device of videoDevices$ | async" [value]="device.deviceId">
+                          {{device.label}}
+                      </mat-option>
+                  </mat-select>
+              </mat-form-field>
+              <button mat-icon-button type="button" (click)="toggleCam()">
+                  <mat-icon [svgIcon]="camIcon()"></mat-icon>
+              </button>
+          </div>
 
-      <mat-form-field>
-        <mat-label>Audio device</mat-label>
-        <mat-select>
-          <mat-option *ngFor="let device of audioInputDevices$ | async" [value]="device.deviceId">
-            {{device.label}}
-          </mat-option>
-        </mat-select>
-      </mat-form-field>
-    </form>
+          <div class="flex gap-2">
+              <mat-form-field class="flex-grow">
+                  <mat-label>Audio device</mat-label>
+                  <mat-select>
+                      <mat-option *ngFor="let device of audioInputDevices$ | async" [value]="device.deviceId">
+                          {{device.label}}
+                      </mat-option>
+                  </mat-select>
+              </mat-form-field>
+              <button mat-icon-button type="button" (click)="toggleAudio()">
+                  <mat-icon [svgIcon]="audioIcon()"></mat-icon>
+              </button>
+          </div>
+      </form>
   `
 })
 export default class RecordComponent implements AfterViewInit {
+  isAudioOn = signal<boolean>(true);
+  isCamOn = signal<boolean>(true);
   mediaRecorder = signal<MediaRecorder | undefined>(undefined);
   recordedBlobs = signal<Blob[]>([]);
   supportedMimeTypes = getSupportedMimeTypes();
 
   videoDevices$ = getVideoDevices();
   audioInputDevices$ = getAudioInputDevices();
+
+  audioIcon = computed(() => this.isAudioOn() ? 'volume' : 'volume_off');
+  camIcon = computed(() => this.isCamOn() ? 'videocam' : 'videocam_off');
 
   @ViewChild('video', {read: ElementRef}) video!: ElementRef<HTMLVideoElement>;
 
@@ -138,5 +156,15 @@ export default class RecordComponent implements AfterViewInit {
       document.body.removeChild(a);
       window.URL.revokeObjectURL(url);
     }, 100);
+  }
+
+  toggleAudio(): void {
+    window.stream.getAudioTracks().forEach((track) => (track.enabled = !this.isAudioOn()));
+    this.isAudioOn.update(isMuted => !isMuted);
+  }
+
+  toggleCam(): void {
+    window.stream.getVideoTracks().forEach((track) => (track.enabled = !this.isCamOn()));
+    this.isCamOn.update(isMuted => !isMuted);
   }
 }
