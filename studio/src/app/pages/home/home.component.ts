7b91cb34be1e545c8c23363082b1aa91bc5ac7d0
Gerome Grignon <gerome.grignon.lp2@gmail.com>
2023-10-08 10:27:14 +0200

    refactor(studio): change logo

--- studio/src/app/pages/home/home.component.ts
+++ studio/src/app/pages/home/home.component.ts
@@ -7,16 +7,16 @@ import {MatButtonModule} from "@angular/material/button";
   standalone: true,
   template: `
     <div class="flex flex-col sm:flex-row items-center justify-center gap-8">
-      <img class="w-80 h-auto" src="assets/images/beaver.jpg" alt=""/>
       <div class="flex flex-col items-center">
-        <h1 class="font-caveat text-8xl text-white">
+        <h1 class="font-caveat text-8xl text-white mb-4">
           BEEVER
         </h1>
-        <p class="font-caveat text-3xl text-white text-center">
+        <p class="text-3xl text-white text-center">
           Your open source
           <span class="whitespace-nowrap">streaming studio</span>
         </p>
       </div>
+      <img class="w-80 h-auto" src="assets/images/logo.png" alt=""/>
     </div>
     <div class="flex justify-center gap-4 py-8">
       <a mat-stroked-button color="accent" routerLink="/record" [queryParams]="{source: 'camera'}">Camera recorder</a>
